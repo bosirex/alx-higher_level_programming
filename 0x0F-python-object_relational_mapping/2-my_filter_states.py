9cf4ea796678e75561f5e07d2cbacd7ddf1a4387
Bosirex <dennis.dancan@gmail.com>
2023-09-17 23:57:29 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     y = db.cursor()
 
-    # Execute the SQL query 
+    # Execute the SQL query
     y.execute("SELECT * \
                  FROM `states` \
                 WHERE BINARY `name` = '{}'".format(sys.argv[4]))
